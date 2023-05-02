99c8a51356ad8ce3e1095313d41e3dcc057ae43a
pedrobl85 <pedrobl1718@gmail.com>
2023-05-02 13:46:25 -0300

    Refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 
-interface Spawn {
+interface ISpawn {
     map: string
     qty: number
     delay1: number
@@ -16,89 +16,120 @@ interface Spawn {
     bossType: number
 }
 
-interface MobInfo {
+interface IMobInfo {
     mob_id: number
     name: string
-    delay1: number
-    delay2: number
     bossType: number
-    spawns: Array<Spawn>
+    spawns: Array<ISpawn>
+    ctr: number
 }
 
-interface DeadMob {
+interface IDeadMob {
     killer: string
-    map_name: string
+    mapname: string
     mob_id: number
     death_time: string
     mob: Object
 }
 
-interface MvpEntry {
+interface IMvpEntry {
     mob_id: number
     mob_name: string
     death_time?: any
     map_name: string
-    qty: number
-    delay1: number
-    delay2: number
-    bossType: number
+    delay1?: number
+    delay2?: number
+    bossType?: number
 }
 
 
-interface IDeadMvpInfo extends Array<DeadMob> { }
-interface IMvpEntries extends Array<MvpEntry> { }
+let allMobInfos: { [index: number]: IMobInfo } = {}
 
-async function getDeadMvpInfo(): Promise<IDeadMvpInfo> {
+
+class MvpStorage {
+    [index: number]: { [index: string]: IMvpEntry }
+}
+
+interface IDeadMvpInfo extends Array<IDeadMob> { }
+interface IMvpEntries extends Array<IMvpEntry> { }
+
+async function getDeadMvp(): Promise<IDeadMvpInfo> {
     const mvpInfo = await fetch("https://api.ragnatales.com.br/mvp")
     return mvpInfo.json()
 }
 
-async function getMobInfo(mob_id: number): Promise<MobInfo> {
+async function getMobInfo(mob_id: number): Promise<IMobInfo> {
     const spawnInfo = await fetch(`https://api.ragnatales.com.br/database/mob?mob_id=${String(mob_id)}`)
-    return spawnInfo.json()
-}
-
-function MvpInfo(mob: MobInfo, deadMob?: DeadMob): IMvpEntries {
-    let listOfMvpInstances: IMvpEntries = []
-    mob.spawns.forEach(item => {
-        const mvp: MvpEntry = {
-            mob_id: mob.mob_id,
-            mob_name: mob.name,
-            map_name: item.map,
-            qty: item.qty,
-            delay1: item.delay1,
-            delay2: item.delay2,
-            bossType: item.bossType,
-            death_time: deadMob ? deadMob.death_time : undefined
-        }
-
-        listOfMvpInstances.push(mvp)
-    })
-    return listOfMvpInstances
-}
-
-async function generateMvpEntries(): Promise<IMvpEntries> {
-    const deadMvps = await getDeadMvpInfo()
-    console.log(deadMvps)
-    let listAllMvpInstances: IMvpEntries = []
-    var allMobInfos: { [index: number]: MobInfo } = {}
-    for (const deadMob of deadMvps) {
-        const mob = await getMobInfo(deadMob.mob_id)
-        allMobInfos[mob.mob_id] = mob
-        const individualMvpInstances = MvpInfo(mob, deadMob)
-        listAllMvpInstances.push.apply(listAllMvpInstances, individualMvpInstances)
+    const mobInfo = await spawnInfo.json()
+    let ctr = 0
+    for (const spawn of mobInfo.spawns) {
+        ctr = ctr + spawn.qty
     }
-    console.log(listAllMvpInstances)
-    console.log(allMobInfos)
-    return listAllMvpInstances
+    mobInfo["ctr"] = ctr
+    return mobInfo
 }
 
-(async () => console.log(await generateMvpEntries()))()
+
+
+async function getDeadMvpInfo(): Promise<any> {
+    const deadMvps = await getDeadMvp()
+    let MvpOcurrences = new MvpStorage()
+    for (const mob of deadMvps) {
+        if (!(mob.mob_id in allMobInfos))
+            allMobInfos[mob.mob_id] = await getMobInfo(mob.mob_id)
+        console.log(mob.mapname)
+        const map = allMobInfos[mob.mob_id].spawns.find(el => el.map = mob.mapname)
+        if (!MvpOcurrences[mob.mob_id]) { MvpOcurrences[mob.mob_id] = {} }
+        MvpOcurrences[mob.mob_id][mob.death_time] = {
+            mob_id: mob.mob_id,
+            mob_name: allMobInfos[mob.mob_id].name,
+            map_name: mob.mapname,
+            delay1: map?.delay1,
+            delay2: map?.delay2,
+            bossType: map?.bossType
+        }
+    }
+    return MvpOcurrences
+}
+
+(async () => console.log(await getDeadMvpInfo()))()
+
+
+
+// async function generateMvpEntries(): Promise<any> {
+//     const deadMvps = await getDeadMvpInfo()
+//     console.log(deadMvps)
+//     let listAllMvpInstances: {[index: number]: IMvpEntries} = {}
+//     for (const deadMob of deadMvps) {
+//         const mob = await getMobInfo(deadMob.mob_id)
+//         allMobInfos[mob.mob_id] = mob
+//         const individualMvpInstances = MvpInfo(mob, deadMob)
+//         listAllMvpInstances[mob.mob_id].push(individualMvpInstances)
+//     }
+//     console.log(listAllMvpInstances)
+//     console.log(allMobInfos)
+//     return listAllMvpInstances
+// }
+
+// (async () => console.log(await generateMvpEntries()))()
 
 
 // async function updateMvpEntries(currentList: IMvpEntries): Promise<IMvpEntries> {
-//     const deadMvps = await getDeadMvpInfo
+//     const deadMvps = await getDeadMvpInfo()
+//     for (const deadMob of deadMvps) {
+//         if (deadMob.mob_id in allMobInfos)
+//     }
+// 
+//     return
 // }
+
+
+
+
+
+
+
+
 
 root.render(
     <React.StrictMode>
