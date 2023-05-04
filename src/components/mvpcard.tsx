479d1e355552b13a9929b55521b8de81e32b5009
pedrobl85 <pedrobl1718@gmail.com>
2023-05-04 16:14:01 -0300

    Add deathtimer functionality to mvpcard component

--- src/components/mvpcard.tsx
+++ src/components/mvpcard.tsx
@@ -3,15 +3,30 @@ import React, { useEffect, useState } from 'react';
 import 'semantic-ui-css/semantic.min.css'
 import { Card, Icon, Image } from 'semantic-ui-react'
 import { IMvpEntry } from '../getinfo'
+import './component.css'
 
 
 
 export default function MvpCard(mobInfo?: IMvpEntry): JSX.Element {
-    function Timer() {
-        const [date, setDate] = useState(moment())
+    function DeathTimer() {
+        const [respawnTimer, setRespawnTimer] = useState<any>(moment())
+        const deathTime = mobInfo?.deathTime.length > 4 ? mobInfo?.deathTime : "NULL"
+        const [possibleFlag, setPossibleFlag] = useState<boolean>(false)
 
         function refreshTimer() {
-            setDate(moment())
+            if (deathTime === "NULL") { setRespawnTimer("Alive") }
+            else {
+                const startPossibleSpawn = moment(deathTime).add(mobInfo?.delay1, "milliseconds")
+                const endPossibleSpawn = startPossibleSpawn.add(mobInfo?.delay2, "milliseconds")
+                const diff = moment.duration(moment().diff(startPossibleSpawn))
+
+                // Checking if MVP is possibly alive
+                if (diff.asMilliseconds() > 0) {setPossibleFlag(true)}
+                else{setPossibleFlag(false)}
+
+                const time = moment.utc(Math.abs(diff.asMilliseconds())).format("HH:mm:ss")
+                setRespawnTimer(time)
+            }
         }
         useEffect(() => {
             const timerIntervalId = setInterval(refreshTimer, 1000)
@@ -20,8 +35,8 @@ export default function MvpCard(mobInfo?: IMvpEntry): JSX.Element {
             }
         }, [])
         return (
-            <span>
-                {date.toLocaleString()}
+            <span className={`timer${possibleFlag ? " possibly_alive": " dead"}`}>
+                {respawnTimer.toString()}
             </span>
         )
     }
@@ -35,14 +50,11 @@ export default function MvpCard(mobInfo?: IMvpEntry): JSX.Element {
                     <span className='date'>{mobInfo === undefined ? "NULL" : mobInfo.mobId}</span>
                 </Card.Meta>
                 <Card.Description>
-                    <Timer />
+                    <DeathTimer />
                 </Card.Description>
             </Card.Content>
             <Card.Content extra>
-                <a>
-                    <Icon name='user' />
-                    22 Friends
-                </a>
+
             </Card.Content>
         </Card>
     );
